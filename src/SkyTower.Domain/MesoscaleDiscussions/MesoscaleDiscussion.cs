558329faee529c73c276c4337d7d675192f617bf
creanium <john@creanium.net>
2026-02-06 22:47:17 -0700

    Building out MD tracking

--- src/SkyTower.Domain/MesoscaleDiscussions/MesoscaleDiscussion.cs
+++ src/SkyTower.Domain/MesoscaleDiscussions/MesoscaleDiscussion.cs
@@ -1,8 +1,60 @@
-﻿using SkyTower.Domain.Abstractions;
+﻿using NetTopologySuite.Geometries;
+using SkyTower.Domain.Abstractions;
 
 namespace SkyTower.Domain.MesoscaleDiscussions;
 
 public sealed class MesoscaleDiscussion : Entity<MesoscaleDiscussion>
 {
-	
+	public DateTimeOffset Issued { get; private set; }
+
+	public DateTimeOffset? Updated { get; private set; }
+
+	public int Year { get; private set; }
+
+	public int Number { get; private set; }
+
+	public Polygon Boundary { get; private set; } = null!;
+
+	public ValidityPeriod ValidityPeriod { get; private set; } = null!;
+
+	public Uri GraphicProductUri { get; private set; } = null!;
+
+	public Uri ImageUri { get; private set; } = null!;
+
+	public string RawContent { get; private set; } = null!;
+
+	public string? AreasAffected { get; private set; }
+
+	public string? Concerning { get; private set; }
+
+	public string? Summary { get; private set; }
+
+	public string? Discussion { get; private set; }
+
+	public static MesoscaleDiscussion Create(
+		DateTimeOffset issued,
+		int number,
+		Polygon boundary,
+		ValidityPeriod validityPeriod,
+		Uri graphicProductUri,
+		Uri imageUri,
+		string rawContent
+	)
+	{
+		var discussion = new MesoscaleDiscussion
+		{
+			Issued = issued,
+			Year = issued.Year,
+			Number = number,
+			Boundary = boundary,
+			ValidityPeriod = validityPeriod,
+			GraphicProductUri = graphicProductUri,
+			ImageUri = imageUri,
+			RawContent = rawContent
+		};
+		
+		// TODO: Raise domain event
+		
+		return discussion;
+	}
 }
---
+++ src/SkyTower.Domain/MesoscaleDiscussions/ValidityPeriod.cs
@@ -0,0 +1,24 @@
+namespace SkyTower.Domain.MesoscaleDiscussions;
+
+public record ValidityPeriod
+{
+	public DateTimeOffset ValidFrom { get; init; }
+	public DateTimeOffset ValidTo { get; init; }
+
+	public ValidityPeriod(DateTimeOffset ValidFrom, DateTimeOffset ValidTo)
+	{
+		if (ValidTo <= ValidFrom)
+		{
+			throw new ArgumentOutOfRangeException(nameof(ValidTo), "ValidTo must be after ValidFrom.");
+		}
+
+		this.ValidFrom = ValidFrom;
+		this.ValidTo = ValidTo;
+	}
+
+	public void Deconstruct(out DateTimeOffset validFrom, out DateTimeOffset validTo)
+	{
+		validFrom = ValidFrom;
+		validTo = ValidTo;
+	}
+}
